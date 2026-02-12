3f5aafd4e828f85d2fbe634a35b01878cf210550
contato <contato@asuaid.com.br>
2026-02-12 15:14:19 +0000

    File changes

--- src/pages/DataImport.jsx
+++ src/pages/DataImport.jsx
@@ -6,6 +6,7 @@ import {
   FileText, Table as TableIcon, X, Eye, Info, HardDrive, AlertTriangle
 } from 'lucide-react';
 import { Link } from 'react-router-dom';
+import { createPageUrl } from '@/utils';
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { Progress } from "@/components/ui/progress";
